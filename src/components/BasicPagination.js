525b39cff4a57416e48c8a7d021e697484af1d78
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-03-02 23:08:45 -0800

    Implement pagenation

---
+++ src/components/BasicPagination.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { withStyles } from "@material-ui/core/styles";
+import Pagination from '@material-ui/lab/Pagination';
+
+const styles = (theme) => ({
+    root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+});
+
+class BasicPagination extends React.Component {    
+    render() {
+        const { classes, length } = this.props;
+        const pages = Math.ceil(length / 10);
+        
+        return (
+            <div className={classes.root}>
+                <Pagination count={pages}  onChange={(event, page) => this.props.getPageNumber(page)} />
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(BasicPagination);
